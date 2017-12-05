6f3207585cb849572a8ca8125542ac28e43a1867
RustyLesh <lyetstudios@gmail.com>
2017-12-05 18:09:35 +1300

    laserscrip change

---
+++ Assets/Scripts/LaserBallAC.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserBallAC : MonoBehaviour {
+
+    LaserHazard laserHazard;
+
+	void Start () {
+        laserHazard = GetComponentInChildren<LaserHazard>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
--- Assets/Scripts/LaserHazard.cs
+++ Assets/Scripts/LaserHazard.cs
@@ -3,22 +3,31 @@ using System.Collections.Generic;
 using UnityEngine;
 
 /*
- * Changes lasers position dynamicly according to a raycast that hits anything in the "Shootable" layer below the attatached object.
+ * Changes lasers position dynamicly according to a 
+ * raycast that hits anything in the "Shootable" layer
+ * below the attatached object.
+ * 
+ * NOTE: If scrip has inbuilt use of "Turn of" and "Turn on" . 
+ * Set downtime to 0 to keep laser on/off.
  */
 public class LaserHazard : MonoBehaviour
 {
     public float maxRange;
+    public float downTime; //delay of laser turning on
+    public float UpTime; // how long laser is on
+    public bool isLaserOn = true;
 
+    Animator anim;
     Transform laserTr;
     SpriteRenderer laserSr;
     BoxCollider2D laserCl;
 
+    float timer;
     int shootableMask;
-
-    float range = 50f;
 
     private void Start()
     {
+        anim = GetComponent<Animator>();
         laserSr = GetComponent<SpriteRenderer>();
         laserCl = GetComponent<BoxCollider2D>();
         shootableMask = LayerMask.GetMask("SolidPlatform");
@@ -26,10 +35,24 @@ public class LaserHazard : MonoBehaviour
 
     private void FixedUpdate()
     {
-        //Math to change the lasers position and size according to hit.
-        if (Physics2D.Raycast(transform.position, -transform.up, range, shootableMask)) //Laser doesn't exist if theres nothing to hit.
+        if (downTime != 0)
         {
-            RaycastHit2D hit = Physics2D.Raycast(transform.position, -transform.up, range, shootableMask);
+            timer += Time.deltaTime;
+
+            if (timer >= downTime && isLaserOn == false)
+            {
+                laserOn();
+            }
+
+            if (timer >= UpTime && isLaserOn == true)
+            {
+                laserOff();
+            }
+        }
+        //Math to change the lasers position and size according to hit.
+        if (Physics2D.Raycast(transform.position, -transform.up, maxRange, shootableMask)) //Laser doesn't exist if theres nothing to hit.
+        {
+            RaycastHit2D hit = Physics2D.Raycast(transform.position, -transform.up, maxRange, shootableMask);
             laserSr.size = new Vector2(laserSr.size.x, hit.distance);
             laserCl.size = new Vector2(laserCl.size.x, laserSr.size.y);
             print("Is Htiing");
@@ -40,5 +63,26 @@ public class LaserHazard : MonoBehaviour
             laserCl.size = new Vector2(laserCl.size.x, laserSr.size.y);
             print("Not Htiing");
         }
+    }
+
+    private void OnTriggerEnter2D()
+    {
+        //TODO: Kill player
+    }
+
+    public void laserOff()
+    {
+        laserSr.color = Color.clear;
+        timer = 0f;
+        isLaserOn = false;
+        laserCl.enabled = false;
+    }
+
+    public void laserOn()
+    {
+        laserSr.color = Color.white;
+        timer = 0f;
+        isLaserOn = true;
+        laserCl.enabled = true;
     }
 }
