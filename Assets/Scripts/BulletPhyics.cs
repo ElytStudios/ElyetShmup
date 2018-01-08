6d79ef77607a7399f59e9eda82987471c6eea06f
RustyLesh <lyetstudios@gmail.com>
2018-01-08 21:57:27 +1300

    new bullet physics scrip

---
+++ Assets/Scripts/BulletPhyics.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletPhyics : MonoBehaviour
+{
+    public float speed;
+    public Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+    // Update is called once per frame
+    void FixedUpdate ()
+    {
+        //rb.AddForce(transform.right * speed);
+        rb.velocity = new Vector2(speed, 0);
+	}
+}
